a759854c6a8197b6a806ee3d08259543ab176c26
Abraham Arias <abraham_arias@epam.com>
2020-11-18 10:45:45 -0700

    moving expectation to test

--- tests/apiTesting/test/first.spec.js
+++ tests/apiTesting/test/first.spec.js
@@ -13,9 +13,18 @@ describe('Users', function () {
         it('Should Get all the users from request', async function () {
             let res = await apiHelper.sendGETRequest(baseUrl, apiEndPoint );
             console.log(res.body);
+
             res.status.should.equal(200);
-            validator.dataTypeValidator(res);
+            const randomItemFromResponse = res.body[Math.floor(Math.random() * res.body.length)];
+
+            randomItemFromResponse.should.have.property('id').which.is.a.Number();
+            randomItemFromResponse.should.have.property('name').which.is.a.String();
+            randomItemFromResponse.should.have.property('username').which.is.a.String();
+            randomItemFromResponse.should.have.property('email').which.is.a.String();
+            randomItemFromResponse.should.have.property('address').which.is.an.object;
+            randomItemFromResponse.should.have.property('phone').which.is.a.String();
+            randomItemFromResponse.should.have.property('website').which.is.a.String();
+            randomItemFromResponse.should.have.property('company').which.is.an.object;
         });
     });
-
 });
